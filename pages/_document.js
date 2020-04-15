a6f2762f63f019f900904a389abe2736cc3bfd5b
Alberico <alberico.zitiello@outlook.it>
2020-04-15 18:17:39 +0200

    favico

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
 const globalStyles = `
 * {
@@ -12,11 +12,31 @@ const globalStyles = `
 export default class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/images/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/images/favicon-16x16.png"
+            sizes="16x16"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          />
           <style type="text/css">{globalStyles}</style>
         </Head>
         <body>
