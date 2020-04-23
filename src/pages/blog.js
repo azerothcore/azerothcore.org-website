bcf05780dea1c475606684ac8860432f73769546
Alberico <alberico.zitiello@outlook.it>
2020-04-23 11:46:22 +0200

    added blog card and preview content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,22 @@
+import { format, parseISO } from 'date-fns';
 import { request } from 'graphql-request';
+import Link from 'next/link';
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { useInView } from 'react-intersection-observer';
+import ReactMarkdown from 'react-markdown';
+import {
+  Button,
+  Card,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+  Row,
+} from 'reactstrap';
 import useSWR from 'swr';
 import Layout from '../components/Layout';
+import { getPreviewText } from '../utils/functions';
 
 const API = 'http://azerothcore.altervista.org/wp/graphql';
 const fetcher = query => request(API, query);
@@ -22,14 +36,13 @@ query Posts {
       id
       content(format: RENDERED)
       author {
-        nickname
+        name
       }
     }
   }
 }`;
 function Blog() {
   const { data, error } = useSWR(query, fetcher);
-  console.log(data);
 
   return (
     <Layout>
@@ -40,9 +53,33 @@ function Blog() {
             {data &&
               data.posts.nodes.map(post => {
                 return (
-                  <Col lg="12">
-                    <div className="post-card" key={post.id}>
-                      <h3>{post.title}</h3>
+                  <Col lg="12" key={post.id}>
+                    <div className="post-card-container">
+                      <Card className="post-card" body>
+                        <CardTitle className="post-card-title">
+                          <h3>{post.title}</h3>
+                        </CardTitle>
+                        <CardSubtitle className="post-card-subtitle">
+                          <span>{`${post.author.name} | ${format(
+                            parseISO(post.date),
+                            'dd LLLL yyyy',
+                          )}`}</span>
+                        </CardSubtitle>
+                        <hr />
+                        <CardText>
+                          <div className="card-preview-text">
+                            <ReactMarkdown
+                              source={getPreviewText(post.content, 300)}
+                              escapeHtml={false}
+                            />
+                          </div>
+                        </CardText>
+                        <Link href="/">
+                          <Button className="post-card-button">
+                            Read the post
+                          </Button>
+                        </Link>
+                      </Card>
                     </div>
                   </Col>
                 );
@@ -52,9 +89,40 @@ function Blog() {
       </div>
       <style jsx>
         {`
-          h3 {
-            font-size: 1.35 rem;
+          .post-card-container {
+            padding: 20px;
           }
+          h3 {
+            font-size: 1.35rem;
+            margin-bottom: 0.3rem;
+          }
+          hr {
+            margin-top: 0.2rem;
+            margin-bottom: 0.7rem;
+          }
+        `}
+      </style>
+      <style jsx global>
+        {`
+          .post-card-title {
+            margin-bottom: 0;
+          }
+          .post-card-subtitle {
+            margin-bottom 10px;
+          }
+          .post-card-subtitle span {
+            color: var(--gray);
+            font-size: 0.9rem;
+          }
+          .post-card-button {
+            margin-left: auto;
+          }
+          .card-preview-text p {
+            margin: 0;
+          }
+          .card-preview-text ol, .card-preview-text li {
+            list-style: none;
+          } 
         `}
       </style>
     </Layout>
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,9 +2,15 @@ import fetch from 'unfetch';
 
 export const fetcher = url => fetch(url).then(r => r.json());
 
-export const cutString = string => {
-  if (string.length > 60) {
-    return `${string.slice(0, 57)}...`;
+export const cutString = (string, length = 60) => {
+  if (string.length > length) {
+    return `${string.slice(0, length - 3)}...`;
   }
   return string;
+};
+
+export const getPreviewText = (string, length) => {
+  const noHtml = string.replace(/<[^>]+>/g, '');
+  const previewString = cutString(noHtml, length);
+  return previewString.toLowerCase();
 };
