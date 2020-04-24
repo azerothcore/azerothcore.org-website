5a77bbcc03fae928dc0505b1b7db22b6a89f7690
breizh24 <breizh24@users.noreply.github.com>
2020-04-24 07:46:43 +0000

    deploy: 79ac72a9b1a5b7e70686fa027e69011ca4ba485f

--- _next/static/FnJv1YwBK34X4NSsM7Vmg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/blog":[a,b,c],"/index":[a,b,c]}}("static\u002Fchunks\u002Fcb1608f2.9ee7bef64331ba2f9b88.js","static\u002Fchunks\u002Fa9a7754c.375d36d1757c81ddb261.js","static\u002Fchunks\u002F85d1f265327b969550ac0f23543f0b2118683753.e49dd5b45aac22552be0.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
