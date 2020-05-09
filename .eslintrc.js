d946e029537a8a958aabf5f39cb599b9d4b74ed5
Alberico <alberico.zitiello@outlook.it>
2020-05-09 17:13:32 +0200

    Updated eslint config to avoid prop types checking when the file is a tsx file since we can use the ts utilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,30 @@
 module.exports = {
   env: {
-    // supporting all kind of environment 
+    // supporting all kind of environment
     // to build universal lib
     browser: true,
     commonjs: true,
     es6: true,
     node: true,
   },
-  plugins: ['json', 'import', 'jsdoc', 'jest', '@typescript-eslint', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'json',
+    'import',
+    'jsdoc',
+    'jest',
+    '@typescript-eslint',
+    'react-hooks',
+  ],
   extends: [
     'eslint:recommended', // basic eslint rules
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'google', // google style guide
-    "plugin:prettier/recommended",
+    'plugin:prettier/recommended',
     'plugin:jest/recommended', // jest support
     'plugin:jest/style', // style for jest files
-    'plugin:jsdoc/recommended', // jsdoc rules to document code 
+    'plugin:jsdoc/recommended', // jsdoc rules to document code
     'prettier',
     'prettier/@typescript-eslint',
     'prettier/react',
@@ -32,9 +40,17 @@ module.exports = {
     // disable eslint jsdoc internal check, already done by "jsdoc" plugin
     'valid-jsdoc': 0,
     'max-len': 0, // disable max length check of code lines
-    'react-hooks/rules-of-hooks': "error",	
-    'react-hooks/exhaustive-deps': "warn",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
   settings: {
     jsdoc: {
       preferredTypes: {
@@ -42,7 +58,7 @@ module.exports = {
       },
     },
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
 };
